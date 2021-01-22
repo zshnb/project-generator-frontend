d6d81a911f3efe57acff5b8e23c260ffc9a71172
zshnb <a857681664@gmail.com>
2021-01-22 16:58:34 +0800

    create config page

---
+++ src/page/ProjectConfig/ProjectConfig.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="project-config">
+    <el-form label-position="left" label-width="180px" :model="config">
+      <el-form-item label="artifactId">
+        <el-input placeholder="artifactId" v-model="config.artifactId"></el-input>
+      </el-form-item>
+      <el-form-item label="groupId">
+        <el-input placeholder="groupId" v-model="config.groupId"></el-input>
+      </el-form-item>
+      <el-form-item label="version">
+        <el-input placeholder="version" v-model="config.version"></el-input>
+      </el-form-item>
+      <el-form-item label="rootPackageName">
+        <el-input placeholder="rootPackageName" v-model="config.rootPackageName"></el-input>
+      </el-form-item>
+      <el-form-item label="entityPackageName">
+        <el-input placeholder="entityPackageName" v-model="config.entityPackageName"></el-input>
+      </el-form-item>
+      <el-form-item label="servicePackageName">
+        <el-input placeholder="servicePackageName" v-model="config.servicePackageName"></el-input>
+      </el-form-item>
+      <el-form-item label="serviceImplPackageName">
+        <el-input placeholder="serviceImplPackageName"
+                  v-model="config.serviceImplPackageName"></el-input>
+      </el-form-item>
+      <el-form-item label="mapperPackageName">
+        <el-input placeholder="mapperPackageName" v-model="config.mapperPackageName"></el-input>
+      </el-form-item>
+      <el-form-item label="controllerPackageName">
+        <el-input placeholder="controllerPackageName"
+                  v-model="config.controllerPackageName"></el-input>
+      </el-form-item>
+      <el-form-item label="jdbcHost">
+        <el-input placeholder="jdbcHost" v-model="config.jdbcHost"></el-input>
+      </el-form-item>
+      <el-form-item label="jdbcPort">
+        <el-input placeholder="jdbcPort" v-model="config.jdbcPort"></el-input>
+      </el-form-item>
+      <el-form-item label="jdbcUser">
+        <el-input placeholder="jdbcUser" v-model="config.jdbcUser"></el-input>
+      </el-form-item>
+      <el-form-item label="jdbcPassword">
+        <el-input placeholder="jdbcPassword" v-model="config.jdbcPassword"></el-input>
+      </el-form-item>
+      <el-form-item label="jdbcDatabase">
+        <el-input placeholder="jdbcDatabase" v-model="config.jdbcDatabase"></el-input>
+      </el-form-item>
+      <el-form-item label="UI">
+        <el-select v-model="config.type">
+          <el-option v-for="item in projectTypes"
+                     :key="item.value"
+                     :label="item.name"
+                     :value="item.value"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProjectConfig",
+    props: {
+      config: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  #project-config
+    .el-form
+      .el-form-item
+        .el-form-item__content
+          width: 300px
+          .el-input
+            width: 300px
+</style>
---
+++ src/page/ProjectEdit/ProjectEdit.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="project-edit">
+    <el-tabs>
+      <el-tab-pane label="基础配置">
+        <project-config :config="config"/>
+      </el-tab-pane>
+      <el-tab-pane label="表结构配置">
+
+      </el-tab-pane>
+      <el-tab-pane label="菜单配置">
+
+      </el-tab-pane>
+      <el-tab-pane label="角色配置">
+
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+  import ProjectConfig from "../ProjectConfig/ProjectConfig";
+
+  export default {
+    name: "ProjectEdit",
+    components: {ProjectConfig},
+    data() {
+      return {
+        config: {
+          groupId: 'com.demo',
+          artifactId: 'demo',
+          version: '1.0',
+          rootPackageName: 'com.demo.demo',
+          entityPackageName: 'entity',
+          servicePackageName: 'service',
+          serviceImplPackageName: 'serviceImpl',
+          mapperPackageName: 'mapper',
+          controllerPackageName: 'controller',
+          jdbcHost: 'localhost',
+          jdbcPort: 3306,
+          jdbcUser: 'root',
+          jdbcPassword: '',
+          jdbcDatabase: '',
+          type: 1
+        },
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+
+</style>
